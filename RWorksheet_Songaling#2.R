53a72e0adb31ce8c6a18f20ea7d406f804502d59
Charles Daniel Songaling <charlesdaniel.songaling@students.isatu.edu.ph>
2023-09-28 04:03:08 +0000

    Worksheet2

---
+++ RWorksheet_Songaling#2.R
@@ -0,0 +1,52 @@
+#1 create a vector using: operator
+  
+  #a.
+  Vector <-(-5:5)
+  Vector
+  #It shows the sequence from -5 to 5.
+  
+  #b.
+  x <- 1:7
+  x
+  
+#2 Create a vector using seq()function
+  vector <-seq(1, 3, by=0.2)
+  vector
+  
+#3 Census
+  ages <-c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27,
+           22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 43,
+           53, 41, 51, 35, 24, 33, 41, 53, 40, 18, 44, 38, 41, 48,
+           27, 39, 19, 30 ,61, 54, 58, 26, 18)
+  
+      #a
+      element_3 <- ages[3]
+      element_3
+  
+      #b
+      element_2_and_4 <-ages[c(2,4)]
+      element_2_and_4
+   
+
+    #c
+    
+#4 Create vector x <-c("first"=3, "second"=0, "third"=9)
+    vectorx <-c("first"=3, "second"=0, "third"=9)
+    vectorx
+    
+      #a
+      vectorx[c("first", "third")]
+      
+      
+#5 Create a sequence from x from -3:2
+    x <-(-3:2)
+    x
+      
+      #a
+      x[2]<- 0
+      x
+  
+#6 Fuel data
+      
+    
+    
